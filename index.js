cb84c69f572d1f529b1b178b2cd5d06887e0c252
YusufByn <yusuf.buyukaydin33@gmail.com>
2026-03-05 11:34:38 +0100

    blob

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ const fragment = document.createDocumentFragment();
 const buttonNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 
-function buttonsCharged(buttonNumbers, divBtns) {
-    buttonNumbers.forEach((buttonNumber) => {
-        const btn = document.createElement("button");
-        btn.textContent(buttonNumber);
-        fragment.appendChild(btn);
-    })
 
-    divBtns.appendChild(fragment)
+function buttonsCharged(buttonNumbers) {
+    buttonNumbers.forEach((buttonNumber) => {
+        console.log(`btn ${buttonNumber}`);
+    })
 }
 
-chargeBtn.addEventListener("click", buttonsCharged(buttonNumbers, divBtns));
+// function buttonsCharged(buttonNumbers, divBtns) {
+//     buttonNumbers.forEach((buttonNumber) => {
+//         const btn = document.createElement("button");
+//         btn.innerText(buttonNumber);
+//         fragment.appendChild(btn);
+//     })
+
+//     divBtns.appendChild(fragment)
+// }
+
+chargeBtn.addEventListener("click", buttonsCharged(buttonNumbers));
 
 
 
